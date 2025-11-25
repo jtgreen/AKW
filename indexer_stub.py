94fe682052954dd49fa5239424b4c82ffe25962b
John Green <john.travis.green@gmail.com>
2025-11-25 06:05:23 -0500

    small change to client line

--- indexer_stub.py
+++ indexer_stub.py
@@ -10,11 +10,9 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 # ------------------- Config & setup -------------------
-
-client = OpenAI(api_key=OPENAI_API_KEY)
-
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+client = OpenAI(api_key=OPENAI_API_KEY)
 
 if not OPENAI_API_KEY:
     raise SystemExit("OPENAI_API_KEY not set in environment/.env")
